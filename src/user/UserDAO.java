013a1a24085cf17b81621096206474b257b8b3fc
COMP1-PC\cph <jiho.develop@gmail.com>
2021-07-09 09:23:38 +0900

    t

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -26,6 +26,24 @@ public class UserDAO {
 
 	}
 	
+//	CREATE TABLE  USER5 (
+//			  userName VARCHAR(20),
+//			  userAge INT,
+//			  userGender VARCHAR(20),
+//			  userEmail VARCHAR(20)
+//			);
+//
+//			INSERT INTO USER5 VALUES ('홍길동', 21, '남자', 'gildong@gmail.com');
+//			INSERT INTO USER5 VALUES ('김철수', 22, '남자', 'chulsu@gmail.com');
+//			INSERT INTO USER5 VALUES ('김영희', 23, '여자', 'younghee@gmail.com');
+//			INSERT INTO USER5 VALUES ('이순신', 24, '남자', 'soonshin@gmail.com');
+//			INSERT INTO USER5 VALUES ('둘리', 25, '남자', 'dooli@gmail.com');
+//			INSERT INTO USER5 VALUES ('Mark', 26, '남자', 'mark@gmail.com');
+//			INSERT INTO USER5 VALUES ('Julia', 27, '여자', 'julia@gmail.com');
+//			INSERT INTO USER5 VALUES ('나이뻐', 28, '여자', 'beauty@gmail.com');
+//			INSERT INTO USER5 VALUES ('이토 히로부미', 29, '남자', 'bastard@gmail.com');
+//			INSERT INTO USER5 VALUES ('안중근', 30, '남자', 'hero@gmail.com');
+
 	public ArrayList<User> search(String userName) {
 		String SQL = "SELECT * FROM USER5 WHERE userName LIKE ?";
 		ArrayList<User> userList = new ArrayList<User>();
